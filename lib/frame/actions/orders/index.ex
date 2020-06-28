6e321d649f6f9b650b49f5fdad377e50f5d41ec6
Stanislav Kravchenko <stankra@yandex.ru>
2020-06-28 18:31:33 +0300

    Use assigns for EEx

--- lib/frame/actions/orders/index.ex
+++ lib/frame/actions/orders/index.ex
@@ -2,11 +2,11 @@ defmodule Frame.Actions.Orders.Index do
   import Plug.Conn
   require EEx
 
-  EEx.function_from_file :def, :index, "lib/frame/templates/posts/index.html.eex", [:period]
+  EEx.function_from_file :def, :index, "lib/frame/templates/posts/index.html.eex", [:assigns]
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    send_resp(conn, 200, index("For today"))
+    send_resp(conn, 200, index(period: "for today"))
   end
 end
