69c8457ae8b637bd9648e5201b72ce50759abb2a
Stanislav Kravchenko <stankra@yandex.ru>
2020-06-28 18:30:16 +0300

    Add EEx

--- lib/frame/actions/orders/index.ex
+++ lib/frame/actions/orders/index.ex
@@ -1,9 +1,12 @@
 defmodule Frame.Actions.Orders.Index do
   import Plug.Conn
+  require EEx
+
+  EEx.function_from_file :def, :index, "lib/frame/templates/posts/index.html.eex", [:period]
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    send_resp(conn, 200, "Orders list")
+    send_resp(conn, 200, index("For today"))
   end
 end
